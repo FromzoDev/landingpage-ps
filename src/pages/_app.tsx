c48becb303eb50ca22780f400fa45b3dc9b7edf0
BenjaminD <benjamin.denis.i2d@gmail.com>
2023-04-26 01:26:54 +0200

    hotfix metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,11 +36,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       {/* <meta name="keywords" content="mots clés, pour, ma, page" /> */}
       <meta property="og:title" content="PhotoStopper" />
       <meta property="og:description" content="🧭 L'application pour redécouvrir le(s) patrimoine(s) des Hauts-de-France" />
-      <meta property="og:image" content="./../../public/Images/metaDataImage.jpg" />
+      <meta property="og:image" content="https://imagedelivery.net/hl1WDbLOjbgv1wkNUhhX3w/3a442acc-5f6f-4897-46e0-ab567b813400/w=7952" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="PhotoStopper" />
       <meta name="twitter:description" content="🧭 L'application pour redécouvrir le(s) patrimoine(s) des Hauts-de-France" />
-      <meta name="twitter:image" content="./../../public/Images/metaDataImage.jpg" />
+      <meta name="twitter:image" content="https://imagedelivery.net/hl1WDbLOjbgv1wkNUhhX3w/3a442acc-5f6f-4897-46e0-ab567b813400/w=7952" />
 
     </Head>
     <Component {...pageProps} />
