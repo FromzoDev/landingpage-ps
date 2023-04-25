c3999deafb31596df36975ba7d38bd1ae0689843
BenjaminD <benjamin.denis.i2d@gmail.com>
2023-04-26 01:23:39 +0200

    hotfix metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 import playstoreButton from "./../../public/Images/BoutonPlayStore.png"
 import applestoreButton from "./../../public/Images/BoutonApple.png"
-
 import Link from 'next/link'
 
 
