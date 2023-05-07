915e6b859f4288c5dd6a71f84c74c12298e4aafa
BenjaminD <benjamin.denis.i2d@gmail.com>
2023-05-07 21:29:04 +0200

    hotfix metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -56,8 +56,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
 
       <Head>
-        <title>Photostopper</title>
-        <link rel="icon" href="https://res.cloudinary.com/dfuhbtvh5/image/upload/v1683281672/logo_pslnmv.png" />
+        <link rel="icon" href="https://res.cloudinary.com/dfuhbtvh5/image/upload/v1683281672/logo_pslnmv.png" type="image/ico"/>
+        <link rel="shortcut icon" href="https://res.cloudinary.com/dfuhbtvh5/image/upload/v1683281672/logo_pslnmv.png"/>
+        <title>PhotoStopper</title>
         <meta name="description"
           content=" 🧭 PhotoStopper, l’application pour (re)découvrir la richesse du patrimoine culturel, architectural, historique et naturel des Hauts-de-France." />
         <meta name="title" content=" PhotoStopper - (Re)découvrez les patrimoines des Hauts-de-France" />
